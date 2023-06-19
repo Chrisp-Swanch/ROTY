6474796aba1cda2ea5922467bc1d092882c5a543
Chris Swan <chrisswan1612@gmail.com>
2023-06-19 16:02:45 +1200

    indexed the 'name' column on users, and rocks tables

--- server/db/migrations/20230614000242_users.js
+++ server/db/migrations/20230614000242_users.js
@@ -3,14 +3,20 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  return knex.schema.createTable('users', function (table) {
-    table.increments('id').primary()
-    table.timestamp('created_at')
-    table.string('name')
-    table.string('profile_image')
-    table.boolean('previous_winner')
-    table.boolean('is_deleted')
-  })
+  return knex.schema
+    .createTable('users', function (table) {
+      table.increments('id').primary()
+      table.timestamp('created_at')
+      table.string('name')
+      table.string('profile_image')
+      table.boolean('previous_winner')
+      table.boolean('is_deleted')
+    })
+    .then(() => {
+      return knex.schema.alterTable('users', function (table) {
+        table.index('name') // Index the name column
+      })
+    })
 }
 
 /**
@@ -18,5 +24,11 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  return knex.schema.dropTable('users')
+  return knex.schema
+    .alterTable('users', (table) => {
+      table.dropIndex('name') // Drop the index from name
+    })
+    .then(() => {
+      return knex.schema.dropTable('users')
+    })
 }
--- server/db/migrations/20230614001051_rocks.js
+++ server/db/migrations/20230614001051_rocks.js
@@ -3,17 +3,23 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  return knex.schema.createTable('rocks', function (table) {
-    table.increments('id').primary()
-    table.timestamp('created_at')
-    table.integer('owner_id')
-    table.string('name')
-    table.string('description')
-    table.string('image')
-    table.string('weight_division')
-    table.boolean('disqualified')
-    table.boolean('is_deleted')
-  })
+  return knex.schema
+    .createTable('rocks', function (table) {
+      table.increments('id').primary()
+      table.timestamp('created_at')
+      table.integer('owner_id')
+      table.string('name')
+      table.string('description')
+      table.string('image')
+      table.string('weight_division')
+      table.boolean('disqualified')
+      table.boolean('is_deleted')
+    })
+    .then(() => {
+      return knex.schema.alterTable('rocks', function (table) {
+        table.index('name')
+      })
+    })
 }
 
 /**
@@ -21,5 +27,11 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  return knex.schema.dropTable('rocks')
+  return knex.schema
+    .alterTable('rocks', (table) => {
+      table.dropIndex('name')
+    })
+    .then(() => {
+      return knex.schema.dropTable('rocks')
+    })
 }
