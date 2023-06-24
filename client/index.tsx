eaed01d3671d5c4e0289f84febaa5f68fb8ae36d
Chris Swan <chrisswan1612@gmail.com>
2023-06-25 10:51:38 +1200

    set up router provider

--- client/index.tsx
+++ client/index.tsx
@@ -2,12 +2,20 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import store from './store'
 
-import App from './components/App'
+import router from './router'
+import { RouterProvider } from 'react-router-dom'
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
+    // <Auth0Provider
+    // domain='facil-joshliu.au.auth0.com'
+    // clientId='LPmXLj6Y5eNaXiEo3BA7MEEwJWNkH2Sq'
+    // redirectUri={window.location.origin}
+    // audience='https://mooo-vies/api'
+    // >
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
+    // </Auth0Provider>
   )
 })
