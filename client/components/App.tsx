0831b90c95f3aa296bf6cf33d2ea36f31f2646e7
chris-swan <93796699+Chrisp-Swanch@users.noreply.github.com>
2023-06-26 12:26:11 +1200

    fetch users from db on app load

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,8 +1,19 @@
 import { Route, Routes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useAppDispatch } from '../hooks/hooks'
+
 import Home from './Home'
 import Nav from './Nav'
+import { getUsersThunk } from '../actions/users'
+
 
 function App() {
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(getUsersThunk())
+  }, [dispatch])
+
   return (
     <>
       <Nav />
