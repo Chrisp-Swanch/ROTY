bd5c0fb88412787da9a2534675817da17416ce79
Chris Swan <chrisswan1612@gmail.com>
2023-06-28 13:44:26 +1200

    add /dist to gitigrnore

--- dist/server/server.js
+++ dist/server/server.js
@@ -4,16 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const path_1 = __importDefault(require("path"));
-const compression_1 = __importDefault(require("compression")); // import compression
+const compression_1 = __importDefault(require("compression"));
 const users_1 = __importDefault(require("./routes/users"));
 const rocks_1 = __importDefault(require("./routes/rocks"));
 const votes_1 = __importDefault(require("./routes/votes"));
 const server = (0, express_1.default)();
-server.use((0, compression_1.default)()); // implement compression
+server.use((0, compression_1.default)());
 server.use(express_1.default.json());
-server.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
 server.use('/api/v1/users', users_1.default);
 server.use('/api/v1/rocks', rocks_1.default);
 server.use('/api/v1/votes', votes_1.default);
+if (process.env.NODE_ENV === 'production') {
+    server.use('/assets', express_1.default.static('../assets'));
+    server.get('*', (req, res) => {
+        res.sendFile('../index.html');
+    });
+}
 exports.default = server;
