8f1cb1caa5d417bb546d91d4b0c890bb1c9dd130
Chris Swan <chrisswan1612@gmail.com>
2023-06-28 13:41:37 +1200

    do some more things

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,6 @@
 import express from 'express'
 import path from 'path'
-import compression from 'compression' // import compression
+import compression from 'compression'
 
 import users from './routes/users'
 import rocks from './routes/rocks'
@@ -8,11 +8,16 @@ import votes from './routes/votes'
 
 const server = express()
 
-server.use(compression()) // implement compression
+server.use(compression())
 server.use(express.json())
-server.use(express.static(path.join(__dirname, 'public')))
 server.use('/api/v1/users', users)
 server.use('/api/v1/rocks', rocks)
 server.use('/api/v1/votes', votes)
 
+if (process.env.NODE_ENV === 'production') {
+  server.use('/assets', express.static('../assets'))
+  server.get('*', (req, res) => {
+    res.sendFile('../index.html')
+  })
+}
 export default server
